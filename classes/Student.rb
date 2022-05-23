9874dd925b2c479a223f87d44ca52ca41f655287
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-05-24 00:16:21 +0200

    Fix linter errors

--- classes/Student.rb
+++ classes/Student.rb
@@ -1,10 +1,11 @@
 require_relative 'person'
 
 class Student < Person
-    def initialize(classroom, age, name = 'unkown', parent_permission: true)
-        @classroom = classroom
-    end
-    def play_hooky
-        '¯\(ツ)/¯'
-    end
+  def initialize(classroom, age, name = 'unkown', parent_permission: true)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
 end
