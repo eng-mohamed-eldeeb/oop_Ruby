04747a0f16ffd456050ea88e76673fff58131ffb
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-05-24 00:52:11 +0200

    Fix all

--- classes/Teacher.rb
+++ classes/Teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(specialization,_id , _age, _name = 'unkown', parent_permission = true)
+  def initialize(specialization, _id, _age, _name = 'unkown', parent_permission: true)
     super
     @specialization = specialization
   end
--- main.rb
+++ main.rb
@@ -1,7 +1,3 @@
 require_relative './classes/person'
 require_relative './classes/student'
 require_relative './classes/teacher'
-
-
-cl = Person.new(25, 18, 'mo')
-print cl.of_age?
