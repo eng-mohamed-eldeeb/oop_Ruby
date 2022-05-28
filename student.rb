60e32638d679d87289cc7e3df5f9d1efa78f74cf
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-05-28 13:17:12 +0200

    Create Classroom

---
+++ student.rb
@@ -0,0 +1,18 @@
+require './Person'
+
+class Student < Person
+  attr_accessor :classroom
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+
+  def add_classroom(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+end
